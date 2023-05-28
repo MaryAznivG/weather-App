5751318124d997cd4836fa2941cfded883f14901
MaryAznivG <marygalstyan10@gmail.com>
2023-05-28 15:27:46 -0700

    took out the celsius temp and fixed my temp

--- src/script.js
+++ src/script.js
@@ -126,33 +126,6 @@ function getCurrentLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(searchLocation);
 }
-
-function displayCelsiusTemperature (event){
-  event.preventDefault();
-  let temperatureElement=document.querySelector("#temperature");
-  celsiusLink.classList.add("active");
-  fahrenheitLink.classList.remove("active");
-  
-  let celsiusTemperature=((fahrenheitTemperature-32)*5)/9;
-  temperatureElement.innerHTML=Math.round(celsiusTemperature);
-}
-
-function displayFahrenheitTemperature(event){
-  event.preventDefault();
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
-  let temperatureElement=document.querySelector("#temperature");
-  temperatureElement.innerHTML=Math.round(fahrenheitTemperature);
-}
-
-let fahrenheitTemperature = null;
-
-let fahrenheitLink=document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
-let celsiusLink=document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click",displayCelsiusTemperature);
-
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
 
